cf5306b13a954d12f4008fd7c2be75566bc4aea4
zengpeng <1769411197@qq.com>
2020-02-12 23:40:30 +0800

    提交String、Hash的常用接口

--- src/main/java/com/pci/hjmos/redis/service/RedisService.java
+++ src/main/java/com/pci/hjmos/redis/service/RedisService.java
@@ -1,38 +1,107 @@
 package com.pci.hjmos.redis.service;
 
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @Component
 public interface RedisService {
-
     /**
+     * --------String--------star
      * 增加接口
+     * @param key 缓存key
+     * @param value 缓存值
      */
     void set(String key, Object value);
 
     /**
+     * 增加接口,附带过期时间
+     * @param key 缓存key
+     * @param value 缓存值
+     * @param expireTime  存活时间(秒s)
+     */
+    void set(String key, Object value, long expireTime);
+
+    /**
+     * 批量添加
+     * @param maps 键值对集合
+     */
+    void multiSet(Map<String, Object> maps);
+
+    /**
      * 获取值接口
+     * @param key 缓存key
      */
     Object get(String key);
 
     /**
+     * 批量获取
+     * @param keys 缓存key集合
+     * @return 值集合
+     */
+    public List<Object> multiGet(Collection<String> keys);
+
+    /**
+     * --------String--------end
      * 删除
+     * @param key 缓存key
      */
     Boolean delete(String key);
 
-
     /**
-     *  修改,同新增接口
+     * --------Hash--------start
+     * 向key的hash 中存hashKey属性值为hashValue
+     * @param key 缓存key
+     * @param hashKey 属性名称
+     * @param  hashValue 属性值
      */
+    void hPut(String key, String hashKey, Object hashValue) ;
+
+    /**
+     * 批量更新hash
+     * @param key hash的key
+     * @param m 属性键值对
+     */
+    void hMultiSet(String key, Map<? extends Object, ? extends Object> m);
+
+    /**
+     * key的hash 中是否存在hashKey属性
+     * @param key 缓存key
+     * @param hashKey 属性名称
+     */
+    Object hGet(String key, String hashKey);
+
+    /**
+     * 删除 key的hash 中hashKey属性
+     * @param key 缓存key
+     * @param hashKey 属性名称
+     */
+    void hRemove(String key, String hashKey);
+
+    /**
+     * 获取 key的hash 中所有属性
+     * @param key 缓存key
+     */
+    Map<Object, Object> hEntries(String key) ;
+
+    /**
+     * * --------Hash--------end
+     * key的hash 中是否存在hashKey属性
+     * @param key 缓存key
+     * @param hashKey 属性名称
+     */
+    Boolean hExists(String key, String hashKey);
 
 
     /**
-     * 设置过期时间 单位（秒）
+     * (通用) 设置过期时间 单位（秒）
      */
     void expireKey(String key, long expireTime);
-
     /**
      *  key的模糊查询
      */
--- src/main/java/com/pci/hjmos/redis/service/impl/RedisServiceImpl.java
+++ src/main/java/com/pci/hjmos/redis/service/impl/RedisServiceImpl.java
@@ -1,11 +1,15 @@
 package com.pci.hjmos.redis.service.impl;
 
 import com.pci.hjmos.redis.service.RedisService;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -22,15 +26,70 @@ public class RedisServiceImpl implements RedisService {
     }
 
     @Override
+    public void set(String key, Object value, long expireTime) {
+        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+        operations.set(key, value);
+        redisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void multiSet(Map<String, Object> maps) {
+        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+        operations.multiSet(maps);
+    }
+
+    @Override
     public Object get(String key) {
         ValueOperations<String, Object> operations = redisTemplate.opsForValue();
         return operations.get(key);
     }
 
     @Override
+    public List<Object> multiGet(Collection<String> keys) {
+        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+        return operations.multiGet(keys);
+    }
+
+    @Override
     public Boolean delete(String key) {
         return redisTemplate.delete(key);
     }
+
+    @Override
+    public void hPut(String key, String hashKey, Object hashValue) {
+        HashOperations<String, Object, Object> operations = redisTemplate.opsForHash();
+        operations.put(key, hashKey, hashValue);
+    }
+
+    @Override
+    public void hMultiSet(String key, Map<?, ?> m) {
+        redisTemplate.opsForHash().putAll(key, m);
+    }
+
+    @Override
+    public Object hGet(String key, String hashKey) {
+        HashOperations<String, Object, Object> operations = redisTemplate.opsForHash();
+        return operations.get(key, hashKey);
+    }
+
+    @Override
+    public void hRemove(String key, String hashKey) {
+        HashOperations<String, Object, Object> operations = redisTemplate.opsForHash();
+        operations.delete(key, hashKey);
+    }
+
+    @Override
+    public Map<Object, Object> hEntries(String key) {
+        HashOperations<String, Object, Object> operations = redisTemplate.opsForHash();
+        return operations.entries(key);
+    }
+
+    @Override
+    public Boolean hExists(String key, String hashKey) {
+        HashOperations<String, Object, Object> operations = redisTemplate.opsForHash();
+        return operations.hasKey(key, hashKey);
+    }
+
 
     @Override
     public void expireKey(String key, long expireTime) {
