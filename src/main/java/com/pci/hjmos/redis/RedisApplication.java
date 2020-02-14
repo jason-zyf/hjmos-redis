7937807f2f8173acc65af0f6ed5570c9d211f360
zhouyunting <jason@163.com>
2020-02-14 09:09:54 +0800

    update

--- src/main/java/com/pci/hjmos/redis/RedisApplication.java
+++ src/main/java/com/pci/hjmos/redis/RedisApplication.java
@@ -5,6 +5,7 @@ import com.pci.hjmos.redis.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,8 @@ import javax.annotation.Resource;
 
 @SpringBootApplication
 @RestController
+@EnableAsync
+//@EnableHystrix
 public class RedisApplication {
 
     public static void main(String[] args) {
