bd4ac9f1ddf3f8a9bb89b4829cd6261a8d4e7685
zhouyunting <jason@163.com>
2020-02-18 00:00:00 +0800

    增加测试从配置中心获取

--- src/main/java/com/pci/hjmos/redis/RedisApplication.java
+++ src/main/java/com/pci/hjmos/redis/RedisApplication.java
@@ -3,10 +3,13 @@ package com.pci.hjmos.redis;
 import com.pci.hjmos.redis.api.RedisApiService;
 import com.pci.hjmos.redis.service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -14,7 +17,7 @@ import javax.annotation.Resource;
 @SpringBootApplication
 @RestController
 @EnableAsync
-//@EnableHystrix
+@EnableHystrix
 public class RedisApplication {
 
     public static void main(String[] args) {
@@ -25,6 +28,18 @@ public class RedisApplication {
     public String index(){
         return "redisIndex";
     }
+
+    // 测试从配置中心获取数据
+    /*@Value("${spring.redis.host}")
+    String host;
+
+    @Value("${spring.redis.port}")
+    String port;
+
+    @RequestMapping(value="/findConfig")
+    public String findConfig(){
+        return host+":"+port;
+    }*/
 
     /*@Resource
     RedisApiService redisService;
