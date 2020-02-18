c137b461e117c64cd846d3aa5d31ed5dbfa76db8
zhouyunting <jason@163.com>
2020-02-18 10:16:18 +0800

    修改名称和包名，将redis改成cache

--- src/main/java/com/pci/hjmos/cache/utils/JsonUtils.java
+++ src/main/java/com/pci/hjmos/cache/utils/JsonUtils.java
@@ -1,4 +1,4 @@
-package com.pci.hjmos.redis.utils;
+package com.pci.hjmos.cache.utils;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
